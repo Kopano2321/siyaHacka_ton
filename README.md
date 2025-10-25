0c9527c283a0f6611ae1d907b5aa08477f0a7f44
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-10-25 19:09:56 +0200

    Add initial Django project structure with essential files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # siyaHacka_ton
+
---
+++ SmartWeatherApp/SmartWeatherApp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for SmartWeatherApp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SmartWeatherApp.settings')
+
+application = get_asgi_application()
