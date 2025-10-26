ebd1ef777b83a9311f4851e8fc017ff6d96ac607
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-10-26 12:16:35 +0200

    Add project overview and key features to README; register models in admin

--- SmartWeatherApp/plants/admin.py
+++ SmartWeatherApp/plants/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CustomUser, Profile, Seasons, Water, Light, Soil, Crops
+
+admin.site.register(CustomUser)
+admin.site.register(Profile)
+admin.site.register(Seasons)
+admin.site.register(Water)
+admin.site.register(Light)
+admin.site.register(Soil)
+admin.site.register(Crops)
