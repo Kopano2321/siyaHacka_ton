dba67ad5b105f88e20e9d6ca5ec036e60c976652
Kopano2321 <kopiyanokb@gmail.com>
2025-10-27 11:09:39 +0200

    Add core app and integrate with main application settings and URLs

--- SmartWeatherApp/SmartWeatherApp/settings.py
+++ SmartWeatherApp/SmartWeatherApp/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "weather_app",
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -135,6 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
